5b0b233761f31f14dbd31534c7865e434736d48b
John Keith <johnkeith@outlook.com>
2014-06-12 16:58:26 -0400

    first user story finished

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,28 @@
+class QuestionsController < ActionController::Base
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new 
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to '/questions'
+    else
+      flash.now[:notice] = "Your question was invalid. Try again!"
+      render :new
+    end
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :description)
+  end
+
+end
